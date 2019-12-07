45afe6dafbef6ec30901b55e7546d5923f4ff1e3
Filip Eriksson <f.eriksson.21.fe@gmail.com>
2019-12-07 01:50:10 +0100

    hämtar nu all öl som matchar sökningen

--- express.js
+++ express.js
@@ -18,26 +18,55 @@ const app = express();
 //	cert: certificate,
 //	ca: ca
 //};
-let beers = {}
-
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    const info = JSON.parse(body);
-    console.log(info.length);
-    beers = info["Hits"];
-  }
-}
-
-
+let beers = [];
+let pageNr = 0;
 
 const options = {
-  url: 'https://api-extern.systembolaget.se/product/v1/product/search?SubCategory=Öl',
+  url: `https://api-extern.systembolaget.se/product/v1/product/search?Page=${pageNr}&SubCategory=Öl`,
   headers: {
     'Ocp-Apim-Subscription-Key': 'acbfd4e32e2b441792e537699cc0efc9'
   }
 };
+// sleep time expects milliseconds
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 
-
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const info = JSON.parse(body);
+      console.log(info["Metadata"]);
+      if(info["Metadata"]["NextPage"] != -1){
+        // set new page
+        pageNr = info["Metadata"]["NextPage"];
+        // updates req url
+        options.url = `https://api-extern.systembolaget.se/product/v1/product/search?Page=${pageNr}&SubCategory=Öl&PriceMax=20`;
+        request(options, callback);
+        // fulfix då systembolaget är noobs och man inte kan välja butik i sin search
+        // jag kan ha fel checka detta senare men for now så funkar det.
+        info["Hits"].forEach(function(beer) {
+          if(beer["IsInStoreSearchAssortment"].includes("0504")){
+            beers.push(beer);
+          }
+        });
+        //beers = beers.concat(info["Hits"]);
+        console.log(beers.length);
+      } else {
+        console.log("Got all beers that met the requirement!");
+      }
+    }
+    if(!error && response.statusCode == 429){
+      const info = JSON.parse(body);
+      // matchar siffran/siffrorna i responsen från bolaget
+      let num = info["message"].match(/\d+/)[0]
+      console.log(info["message"]);
+      // sover och testar hämta ny info
+      sleep(num * 1000).then(() => {
+        console.log("Sleep done! Gathering more Beer :)");
+        request(options, callback);
+      });
+    }
+}
 
 app.get('/sys', function (req, res) {
     res.send(beers[Math.floor(Math.random() * (beers.length + 1))])
@@ -51,6 +80,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(80, () => {
 	console.log('HTTP Server running on port 80');
+  // hämta info från systembolaget
   request(options, callback)
 });
 
